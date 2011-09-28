d2c5403763b278d83a8e27907f0b365361868406
Ben Van Treese <ben@dropecho.com>
2011-09-27 21:39:26 -0500

    working on spatial query stuff, getting started on my octree class

---
+++ include/desteer/spatial/octree.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "desteer/entity/IBaseEntity.hpp"
+#include <vector>
+
+namespace desteer{
+namespace spatial{
+
+class octree
+{
+private:
+    std::vector<entity::IBaseEntity *> _indices;
+    octree[8] _children;
+    int _maxDepth;
+    int _maxIndices;
+    float _size;
+
+public:
+    octree(int maxDepth = 8, int maxIndices = 36, float size = 256.0f);
+    void insert(entity::IBaseEntity* item);
+    void remove(entity::IBaseEntity* item);
+
+    void recalculateIndices();
+    entity::IBaseEntity* findNeighbors(entity::IBaseEntity* item);
+};
+
+
+} //end namespace spatial
+} //end namespace desteer
---
+++ source/octree.cpp
@@ -0,0 +1,54 @@
+#include "desteer/spatial/octree.hpp"
+
+using namespace desteer;
+using namespace spatial;
+using namespace entity;
+
+using namespace std;
+
+octree::octree(int maxDepth, int maxIndices, float size)
+{
+    children = 0;
+}
+
+void octree::insert(IBaseEntity *item)
+{
+    //If already split, add item to a leaf node...
+    if(_children)
+    {
+        // Determine which node to add index to.
+
+        // Add index to correct node;
+    }
+    // else simply add it here.
+    else
+    {
+        // Split the tree when it has too many indices.
+        if(_indices.size() +1 >= _maxIndices)
+        {
+            _children = octree[8];
+            // Recurse back onto this, as the chilren should now be existant.
+            insert(item);
+        }
+        //just add the item if its still below the max.
+        else
+        {
+            _indices.push_back(item);
+        }
+    }
+}
+
+void octree::remove(IBaseEntity *item)
+{
+
+}
+
+void octree::recalculateIndices()
+{
+
+}
+
+IBaseEntity* octree::findNeighbors(IBaseEntity* item)
+{
+
+}
